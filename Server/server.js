00a6c495ee88d2d5312c6ddc26105d018e6453d8
BHASVIC-BeaRobb22 <137883200+BHASVIC-BeaRobb22@users.noreply.github.com>
2023-10-06 00:33:31 +0100

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/Client/mainSCREEN.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080!');
+});
