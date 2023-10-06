3cc8de051518a964d12106d1d8a5d883e8886c4b
BHASVIC-BeaRobb22 <bea.robb22@students.bhasvic.ac.uk>
2023-10-06 13:58:35 +0100

    Stage 1 Setup
    - Set up server
    - Serves mainSCREEN.html
    - styles.css shown
    - socket ID displayed in console

--- Server/server.js
+++ Server/server.js
@@ -4,17 +4,22 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const path = require("path");
+const backwards = path.join(__dirname, '../'); // points to "/Connect 4 Project"
 
+
+app.use(express.static(path.join(backwards, "/Client/"))); // allows CSS to be displayed when serving mainSCREEN.html
 
 app.get('/', (req, res) => {
-
-  res.sendFile(__dirname + '/Client/mainSCREEN.html');
+  res.sendFile(backwards + '/Client/mainSCREEN.html'); // serves mainSCREEN.html
 });
 
-io.on('connection', (socket) => {
-  console.log(socket.id);
+io.on('connection', (socket) => { 
+  console.log(socket.id); // Displays socket ID of connected client
 });
 
 server.listen(8080, () => {
   console.log('Server running on port 8080!');
 });
+
+
