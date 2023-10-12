a1472226a3adea444275ecac5998591e09713703
BHASVIC-BeaRobb22 <bea.robb22@students.bhasvic.ac.uk>
2023-10-12 10:01:44 +0100

    fixing the join button

--- Client/mainCLIENT.js
+++ Client/mainCLIENT.js
@@ -1,1 +1,9 @@
-const socket = (io);
+var socket = (io);
+
+const joinButton = document.getElementById("joinButton");
+
+
+joinButton.addEventListener("click", () => {
+    window.location.href("joinSCREEN.html");
+})
+
