2823113b39863abbf8a6c46d78053298a67ea09e
BHASVIC-BeaRobb22 <bea.robb22@students.bhasvic.ac.uk>
2023-10-19 09:41:27 +0100

    Redirecting

--- Client/mainCLIENT.js
+++ Client/mainCLIENT.js
@@ -1,8 +1,13 @@
 var socket = (io);
 
 const joinButton = document.getElementById("joinRoom");
+const joinToMain = document.getElementById("joinToMain");
+
 
 joinButton.addEventListener("click", () => {
     window.location.href = "joinSCREEN.html";
 })
 
+joinToMain.addEventListener("click", () => {
+    window.location.href = "mainSCREEN.html";
+})
