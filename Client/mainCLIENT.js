d6bcaad701469492edb45c419af0600e09a9329d
BHASVIC-BeaRobb22 <bea.robb22@students.bhasvic.ac.uk>
2023-11-23 09:53:15 +0000

    updated comment
    updated roomForm !== null code comment

--- Client/mainCLIENT.js
+++ Client/mainCLIENT.js
@@ -8,13 +8,15 @@ function redirectToMain() {
 
 let roomForm = document.getElementById("roomForm");
 
-if (roomForm !== null) { // checks that the element does exist (i.e. joinSCREEN loaded)
+if (roomForm !== null) { // checks that the element does exist (i.e. joinSCREEN loaded) so multiple pages can be handled without the listener causing errors
     roomForm.addEventListener("submit", (e) => {
         e.preventDefault(); // stops default function of submit button
         var roomJoin = document.getElementById("roomCodeJoin").value;
         socket.emit("roomJoinToServer", roomJoin); // sends roomJoin variable (room code) to server for validation
     }
 )}
+
+// These two socket.ons are used to display an error message if the user does not enter a code 6 chars in length
 
 socket.on("roomJoin0Length", message => {
     document.getElementById("joinError").innerHTML = message;
